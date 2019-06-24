d67248b470c07c057d6a51a275489f672dc224ea
sebug <sebastian.gfeller@gmail.com>
2019-06-24 16:43:44 +0200

    Auto-filling date

--- client/message-log/message-log/wwwroot/js/site.js
+++ client/message-log/message-log/wwwroot/js/site.js
@@ -2,3 +2,39 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+(function () {
+    function customizeMessagePage() {
+        const enteredOnElement = document.querySelector('#EnteredOn');
+        if (enteredOnElement) {
+            enteredOnElement.addEventListener('focus', event => {
+                if (!event.target.value) {
+                    const currentDate = new Date();
+                    const year = currentDate.getFullYear();
+                    let month = currentDate.getMonth() + 1;
+                    if (month.toString().length == 1) {
+                        month = '0' + month;
+                    }
+                    let day = currentDate.getDate() + 1;
+                    if (day.toString().length == 1) {
+                        day = '0' + day;
+                    }
+                    let hour = currentDate.getHours();
+                    if (hour.toString().length == 1) {
+                        hour = '0' + hour;
+                    }
+                    let minute = currentDate.getMinutes();
+                    if (minute.toString().length == 1) {
+                        minute = '0' + minute;
+                    }
+                    event.target.value = year + '-' +
+                        month + '-' + day + ' ' +
+                        hour + ':' + minute;
+                }
+            });
+        }
+    }
+
+    if (location.href.toLowerCase().indexOf("message") >= 0) {
+        customizeMessagePage();
+    }
+}());
