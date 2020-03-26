f576f1b0f264c41418f37ebce3e9d8deef158bea
sebug <sebastian.gfeller@gmail.com>
2020-03-26 09:54:15 +0100

    Even authenticating log stream

--- client/message-log/message-log/Pages/LogStream.cshtml.cs
+++ client/message-log/message-log/Pages/LogStream.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using message_log.Models;
 using message_log.Repositories;
@@ -13,19 +14,42 @@ namespace message_log.Pages
     {
         private readonly IMessageRepository _messageRepository;
         private readonly IEventRepository _eventRepository;
+        private readonly IAuthenticationService _authenticationService;
         public LogStreamModel(IMessageRepository messageRepository,
-            IEventRepository eventRepository)
+            IEventRepository eventRepository,
+            IAuthenticationService authenticationService)
         {
             this._messageRepository = messageRepository;
             this._eventRepository = eventRepository;
+            this._authenticationService = authenticationService;
         }
 
         public List<Message> Messages { get; set; }
 
         public string EventName { get; set; }
 
-        public void OnGet(int eventID)
+        private bool CheckAuthentication()
         {
+            string username = this.Request.Cookies["username"];
+            string password = this.Request.Cookies["password"];
+
+            if (String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password))
+            {
+                return false;
+            }
+            var passwordBytes = Convert.FromBase64String(password);
+            password = Encoding.UTF8.GetString(passwordBytes);
+
+            return this._authenticationService.IsAuthenticated(username, password);
+        }
+
+        public IActionResult OnGet(int eventID)
+        {
+            if (!this.CheckAuthentication())
+            {
+                return this.Redirect("/Login?eventID=" + eventID);
+            }
+
             var msgs = this._messageRepository.GetAllByEventID(eventID);
             msgs = msgs.OrderByDescending(msg => msg.EnteredOn);
             this.Messages = msgs.ToList();
@@ -36,6 +60,8 @@ namespace message_log.Pages
                 this.EventName = ev.EventName;
                 this.ViewData["Title"] = this.EventName;
             }
+
+            return null;
         }
     }
 }
